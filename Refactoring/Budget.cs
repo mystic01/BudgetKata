61b23395ba1824f10368f93eefe115567fa7a783
Mystic <mysteriousGE@gmail.com>
2018-05-30 20:24:41 +0800

    Remove all unused code in Budget.

--- Refactoring/Budget.cs
+++ Refactoring/Budget.cs
@@ -1,30 +1,9 @@
-﻿using System;
-
-namespace GOOS_Sample.Models
+﻿namespace GOOS_Sample.Models
 {
     public class Budget
     {
         public string YearMonth { get; set; }
 
         public long Amount { get; set; }
-
-        public int Month
-        {
-            get { return Convert.ToInt32(YearMonth.Substring(YearMonth.Length - 2)); }
-        }
-
-        public decimal AverageBudget
-        {
-            get { return (decimal)Amount / DaysInMonth; }
-        }
-
-        public int DaysInMonth
-        {
-            get
-            {
-                var year = Convert.ToInt32(YearMonth.Substring(0, 4));
-                return DateTime.DaysInMonth(year, Month);
-            }
-        }
     }
 }
